5777062eb0808e2a17afff30ab0213f879f7b4f8
shyam agarwal <007shyamagarwal@gmail.com>
2016-12-21 11:18:31 +0530

    first commit for hadoop

---
+++ InvertedIndex/src/InvertedIndex.java
@@ -0,0 +1,66 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapred.TextInputFormat;
+//import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class InvertedIndex {
+
+	
+	public static void main(String[] args) throws Exception {
+		 
+		Configuration conf= new Configuration();
+		 
+		Job job = Job.getInstance(conf,"inverted count");
+		 
+		//Defining the output key and value class for the mapper
+		 
+		job.setMapOutputKeyClass(Text.class);
+		 
+		job.setMapOutputValueClass(Text.class);
+		 
+		job.setJarByClass(InvertedIndex.class);
+		 
+		job.setMapperClass(Map.class);
+		 
+		job.setReducerClass(Reduce.class);
+		 
+		//Defining the output value class for the mapper
+		 
+		job.setOutputKeyClass(Text.class);
+		 
+		job.setOutputValueClass(Text.class);
+		 
+		job.setInputFormatClass(TextInputFormat.class);
+		 
+		job.setOutputFormatClass(TextOutputFormat.class);
+		 
+		Path outputPath = new Path(args[1]);
+		 
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		 
+		FileOutputFormat.setOutputPath(job, outputPath);
+		 
+		//deleting the output path automatically from hdfs so that we don't have delete it explicitly
+		 
+		outputPath.getFileSystem(conf).delete(outputPath);
+		 
+		//exiting the job only if the flag value becomes false
+		 
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		 
+		}
+		 
+		
+}
+	
+	
+	
+
---
+++ WordCount/src/WordCountCombiner.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+ class WordCountCombiner extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values,
+                          Context context)
+            throws IOException, InterruptedException {
+        System.out.println("Entering WordCountCombiner.reduce() " );
+        int sum = 0;
+        Iterator<IntWritable> valuesIt = values.iterator();
+        while(valuesIt.hasNext()){
+            sum = sum + valuesIt.next().get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+}
